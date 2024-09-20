a6042d9b0796b04624b23e72c2ae2d69630c7723
Leevi Tulijoki <leevi.tulijoki@outlook.com>
2024-09-20 18:16:56 +0300

    lisätty viestin lähetys

---
+++ public/scripts.js
@@ -0,0 +1,19 @@
+document.getElementById('send-button-chatbox').addEventListener('click', sendMessage)
+
+document.getElementById('user-input-chatbox').addEventListener('keypress', e => e.key == 'Enter' && sendMessage())
+
+function sendMessage() {
+  console.log('Viesti lähetetty')
+  const input = document.getElementById('user-input-chatbox');
+  const userMessage = input.value;
+  input.value = '';
+  console.log(userMessage)
+  addMessageToChat(userMessage)
+}
+
+function addMessageToChat(message) {
+  const messageElement = document.createElement('div')
+  messageElement.textContent = message
+  console.log(messageElement)
+  document.getElementById('chatbox').appendChild(messageElement)
+}
