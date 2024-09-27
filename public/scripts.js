f0de52e29a18220f2b35e0310f5df0f8cc82489a
Leevi Tulijoki <leevi.tulijoki@outlook.com>
2024-09-27 18:15:21 +0300

    lisätty lähetys palvelimelle

--- public/scripts.js
+++ public/scripts.js
@@ -2,13 +2,21 @@ document.getElementById('send-button-chatbox').addEventListener('click', sendMes
 
 document.getElementById('user-input-chatbox').addEventListener('keypress', e => e.key == 'Enter' && sendMessage())
 
-function sendMessage() {
+async function sendMessage() {
   console.log('Viesti lähetetty')
   const input = document.getElementById('user-input-chatbox');
   const userMessage = input.value;
   input.value = '';
   console.log(userMessage)
   addMessageToChat(userMessage)
+
+  const response = await fetch('/ekatesti', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ question: userMessage })
+  })
 }
 
 function addMessageToChat(message) {
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 
 const app = express()
 const port = 3000
 
 app.use(express.static('public'))
+app.use(bodyParser.json())
+
+app.post('/ekatesti', (req, res) => {
+  const userMessage = req.body.question;
+  console.log("Käyttäjä lähetti backendille viestin: " + userMessage)
+})
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`)
